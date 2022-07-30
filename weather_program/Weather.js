d15f9a6199eaa8da5c24b52bc020ad2c54cadcf6
delphine <delphine.mubiligi@gmail.com>
2022-07-30 01:23:38 +0100

    updated displayWeather function with celsius and percent symbols

--- weather_program/Weather.js
+++ weather_program/Weather.js
@@ -19,9 +19,9 @@ class Weather {
   displayWeather() {
     console.log(`City:\t\t${this.weatherData.name}`)
     console.log(`Weather:\t${this.weatherData.weather[0].main}`)
-    console.log(`Temperature:\t${this.weatherData.main.temp}`)
-    console.log(`Feels like:\t${this.weatherData.main.feels_like}`)
-    console.log(`Feels like:\t${this.weatherData.main.humidity}`)
+    console.log(`Temperature:\t${this.weatherData.main.temp}°C`)
+    console.log(`Feels like:\t${this.weatherData.main.feels_like}°C`)
+    console.log(`Humidity:\t${this.weatherData.main.humidity}%`)
   }
 }
 
